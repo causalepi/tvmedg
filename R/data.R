b4cb44ca293aec6d18e8d0755313daed1174c776
The Nguyen <143731553+pntnguyen@users.noreply.github.com>
2025-06-11 10:32:03 +0700

    simdata

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' @title Simulation dataset
+#' @description This is a simulation dataset
+#' @format A data frame with 1147 rows and 13 variables:
+#' \describe{
+#'   \item{\code{id}}{integer patient id}
+#'   \item{\code{mm}}{double time variable}
+#'   \item{\code{Ap}}{double exposure variable}
+#'   \item{\code{Mp}}{double mediator variable}
+#'   \item{\code{L1}}{double time-varying variable}
+#'   \item{\code{L2}}{double time-varying variable}
+#'   \item{\code{L3}}{double time-varying variable}
+#'   \item{\code{Yp}}{double outcome variable}
+#'   \item{\code{age}}{double age variable}
+#'   \item{\code{sex}}{integer sex variable}
+#'   \item{\code{ow}}{integer overweight}
+#'   \item{\code{risk}}{integer risk}
+#'   \item{\code{lastid}}{double last time follow up}
+#'}
+#' @details DETAILS
+"sim_data"
